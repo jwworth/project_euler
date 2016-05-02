012895eebeb516cf62f17bc35ba07e1ec0682bb6
Jake Worth <jakeworth82@gmail.com>
2016-05-02 12:46:21 -0500

    Solve problem #7

---
+++ 007.rb
@@ -0,0 +1,20 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see 
+# that the 6th prime is 13.
+
+# What is the 10 001st prime number?
+
+require_relative 'primality.rb'
+require 'benchmark'
+
+def large_prime_number(limit)
+  primes = 1
+  count = 2
+  while primes < limit
+    count += 1
+    primes += 1 if is_prime?(count)
+  end
+  count
+end
+
+puts "Answer: #{ large_prime_number(10001) }"
+puts "Time: #{ Benchmark.measure { large_prime_number(10001) } }"
