c335fd9e8f59500f311a26350446457bc5154eaf
Jake Worth <jakeworth82@gmail.com>
2016-05-02 12:46:21 -0500

    Add Benchmarking, user-friendly explanations, and utilize second and third args

--- 001.rb
+++ 001.rb
@@ -3,11 +3,13 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+require 'benchmark'
+
 def sum_of_multiples(limit, value_a, value_b)
   sum = 0
-  (1...limit).each { |n| sum += n if n % 3 == 0 || n %5 == 0 }
+  (1...limit).each { |n| sum += n if n % value_a == 0 || n % value_b == 0 }
   sum
 end
 
-puts sum_of_multiples(10, 3, 5)
-puts sum_of_multiples(1000, 3, 5)
+puts "Answer: #{ sum_of_multiples(1000, 3, 5) }"
+puts "Time: #{ Benchmark.measure {sum_of_multiples(1000, 3, 5) } }"
