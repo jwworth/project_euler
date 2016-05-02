3353add7d63c745101be8193aef1660bb911880b
Jake Worth <jakeworth82@gmail.com>
2016-05-02 12:46:21 -0500

    Add a function for determining primality

---
+++ primality.rb
@@ -0,0 +1,8 @@
+# Determine primality 
+
+def is_prime?(number)
+  return false if number < 2
+  prime = nil
+  (2..(Math.sqrt(number))).each { |n| prime = false if number % n == 0 }
+  prime == false ? false : true
+end
